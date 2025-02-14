457af51665863cd96d02f909f46c36032a24a2d4
qhjiang <1155228873@link.cuhk.edu.hk>
2025-02-14 11:31:51 +0800

    Update checkValid.m

--- organized_sectionB/1155157980/checkValid.m
+++ organized_sectionB/1155157980/checkValid.m
@@ -1,31 +1,31 @@
-% function v = checkValid(M,i,j)
-%     v = 1;
-%     errorcase = 0;
-%     if (i ~= 1) & (i ~= 2) & (i ~= 3)
-%         errorcase = 1;
-%     end
-%     if (j ~= 1) & (j ~= 2) & (j ~= 3)
-%         errorcase = 2;
-%     end
-%     if errorcase ==0
-%         if M(i,j) ~= 0
-%             errorcase = 3;
-%         end
-%     end
-%     if errorcase == 1
-%         disp('Invalid i');
-%         v = 0;
-%     end
-%     if errorcase == 2
-%         disp('Invalid j');
-%         v = 0;
-%     end
-%     if errorcase == 3
-%         disp('Already occupied');
-%         v = 0;
-%     end
-%     if v == 1
-%         disp('Valid input');
-%     end
-% end
-% 
+function v = checkValid(M,i,j)
+    v = 1;
+    errorcase = 0;
+    if (i ~= 1) & (i ~= 2) & (i ~= 3)
+        errorcase = 1;
+    end
+    if (j ~= 1) & (j ~= 2) & (j ~= 3)
+        errorcase = 2;
+    end
+    if errorcase ==0
+        if M(i,j) ~= 0
+            errorcase = 3;
+        end
+    end
+    if errorcase == 1
+        disp('Invalid i');
+        v = 0;
+    end
+    if errorcase == 2
+        disp('Invalid j');
+        v = 0;
+    end
+    if errorcase == 3
+        disp('Already occupied');
+        v = 0;
+    end
+    if v == 1
+        disp('Valid input');
+    end
+end
+

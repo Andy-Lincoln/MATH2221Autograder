f58a7b5283624365167b4aeb9f85d25505c9e690
qhjiang <1155228873@link.cuhk.edu.hk>
2025-02-21 17:43:00 +0800

    uploaded from workPC

--- organized_sectionC/1155199403/chutes_and_ladders.m
+++ organized_sectionC/1155199403/chutes_and_ladders.m
@@ -8,7 +8,19 @@ function n = chutes_and_ladders()
 
     position = 1; % Start at Box 1
     n = 0; % Count number of dice rolls
-    
+    global INPUT_VALUES INPUT_COUNTER;
+    INPUT_VALUES = [5 5 5 1 3 4 6 6 6 4 4 6 1 6 3 1 5 1 1 4 2 2 3 3 3 4 2 2 1 2 5 2 5 5 5 5 2 2 4 2 5 5 4 2 5 2 3 3 4 6 5 6 2 4 1 5 4 6 6 6 3];
+    INPUT_COUNTER = 1;
+    function val = mock_randi(~)
+
+        if INPUT_COUNTER > length(INPUT_VALUES)+1
+            error('Ran out of test inputs - possible infinite loop');
+        end
+        val = INPUT_VALUES(INPUT_COUNTER);
+        INPUT_COUNTER = INPUT_COUNTER + 1;
+    end
+
+    randi = @mock_randi;
     while position ~= 100
         % Roll a fair six-sided die
         d = randi([1, 6]);
